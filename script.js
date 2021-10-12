09bce4fc54154210c8d1ec49992ec8415405eeb4
PattyOlvera <paty.orion@outlook.com>
2021-10-12 13:43:13 -0500

    Save the guess history in a variable called guess

--- script.js
+++ script.js
@@ -6,13 +6,13 @@
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * Done: Complete the showYouWon, showNumberAbove, showNumberBelow
  * Done: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
+ * Done: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses 
-// let numberGuess = 0;
+let guesses = [];
 // Variable for store the correct random number 
 let correctNumber = getRandomNumber();
 
@@ -28,9 +28,8 @@ window.onload = function() {
 function playGame(){
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById('number-guess').value;
-  console.log('Number Guess ' + numberGuess);
   displayResult(numberGuess);
-  
+  saveGuessHistory(numberGuess);  
 }
 /**
  * Show the result for if the guess it too high, too low, or correct
@@ -81,6 +80,8 @@ function getRandomNumber(){
  */
 function saveGuessHistory(guess) {
   // *CODE GOES BELOW HERE *
+  guesses.push(guess);
+  console.log(guesses);
 }
 
 /**
